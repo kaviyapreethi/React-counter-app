1958425c7e8958de29bae1edf4e6df314b767b51
kaviyapreethi <preethikaviya97@gmail.com>
2020-01-02 18:11:46 +0530

    Conditional Rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,12 +3,23 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
-    tags: ["tag1", "tag2", "tag3"]
+    tags: []
   };
   styles = {
     fontSize: 10,
     fontWeight: "bold"
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
   render() {
     return (
       <React.Fragment>
@@ -21,11 +32,8 @@ class Counter extends Component {
         >
           Increment
         </button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
       </React.Fragment>
     );
   }
