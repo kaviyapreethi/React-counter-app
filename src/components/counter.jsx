ba0148d2279a841f9ab5293bb401cef6dee4852b
kaviyapreethi <preethikaviya97@gmail.com>
2020-01-02 18:21:55 +0530

    Handling Events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -20,6 +20,10 @@ class Counter extends Component {
       </ul>
     );
   }
+
+  handleIncrement() {
+    console.log("Increment Clicked");
+  }
   render() {
     return (
       <React.Fragment>
@@ -27,12 +31,13 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button
+          onClick={this.handleIncrement}
           style={{ fontStyle: "italic" }}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.state.tags.length === 0 && " Please create a new tag!"}
         {this.renderTags()}
       </React.Fragment>
     );
